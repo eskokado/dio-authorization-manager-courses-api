82732b74ef176a58e8c6ef40cf51a25047dadc8c
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-27 15:18:03 -0300

    Create controller UsersController

---
+++ curso-api/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using curso_api.Models.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso_api.Controllers
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(RegisterViewModelInput registerViewModelInput)
+        {
+            return Created("", registerViewModelInput);
+        }
+    }
+}
